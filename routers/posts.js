c293cf291850d84a2dd162dc9695f645c74ae976
nahvego <nahvergon@gmail.com>
2018-06-01 21:24:43 +0200

    Optimizada consulta de búsqueda de posts.

--- routers/posts.js
+++ routers/posts.js
@@ -14,11 +14,11 @@ const router = require('express').Router();
 module.exports = router;
 
 /* QUERIES QUE SE REUTILIZAN */
+
 /*
 ---
-*/
-const buildPostQuery = function(req) {
-	return "" +
+Iteración anterior:
+return "" +
 	"SELECT posts.*, users.name AS authorName, IFNULL(votes.score, 0) AS score, " +
 	"COUNT(DISTINCTROW comments.id) AS commentCount, IFNULL(COUNT(DISTINCT just_upvotes.id)*100/totalVotes, 0) AS upvotePercentage FROM `posts` " + 
 	"LEFT JOIN `users` ON posts.authorID = users.id " +
@@ -26,6 +26,22 @@ const buildPostQuery = function(req) {
 	"LEFT JOIN (SELECT postID, SUM(value) AS score, COUNT(*) AS totalVotes FROM `post_votes` GROUP BY postID) votes ON posts.id = votes.postID " +
 	"LEFT JOIN `post_votes` AS just_upvotes ON posts.id = just_upvotes.postID AND just_upvotes.value > 0 " +
 	"WHERE posts.subID = ? " + 
+	(req.post ? "AND posts.id = ? " : "") + 
+	(req.options.fromID ? "AND posts.id < '" + req.options.fromID + "' " : "") + 
+	"GROUP BY posts.id ORDER BY posts.id DESC LIMIT " + req.options.count;
+*/
+const buildPostQuery = function(req) {
+	return "" +
+	"SELECT posts.*, users.name AS authorName, COUNT(DISTINCTROW comments.id) AS commentCount, " +
+	"COUNT(DISTINCT votes.id) AS totalVotes, " +
+	"IFNULL(SUM(votes.value)*COUNT(DISTINCT votes.id)/COUNT(posts.id), 0) AS score, " +
+	"IFNULL(COUNT(DISTINCT just_upvotes.id)*100/COUNT(DISTINCT votes.id), 0) AS upvotePercentage " +
+	"FROM `posts` " +
+	"LEFT JOIN `users` ON posts.authorID = users.id " +
+	"LEFT JOIN `comments` ON posts.id = comments.postID " +
+	"LEFT JOIN `post_votes` AS votes ON posts.id = votes.postID " +
+	"LEFT JOIN `post_votes` AS just_upvotes ON posts.id = just_upvotes.postID AND just_upvotes.value > 0 " +
+	"WHERE posts.subID = ?  " +
 	(req.post ? "AND posts.id = ? " : "") + 
 	(req.options.fromID ? "AND posts.id < '" + req.options.fromID + "' " : "") + 
 	"GROUP BY posts.id ORDER BY posts.id DESC LIMIT " + req.options.count;
